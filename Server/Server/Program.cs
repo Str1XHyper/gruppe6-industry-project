6fe82163ef7ef605e92b099fd2e528276f1c6e12
Str1XHyper <tijnvanveghel@gmail.com>
2021-11-29 13:15:26 +0100

    Setup base socket server

--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using WebSocketSharp;
+using WebSocketSharp.Server;
 
 namespace Server
 {
+    class SocketServer : WebSocketBehavior
+    {
+        protected override void OnClose(CloseEventArgs e)
+        {
+            base.OnClose(e);
+        }
+
+        protected override void OnError(ErrorEventArgs e)
+        {
+            base.OnError(e);
+        }
+
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            base.OnMessage(e);
+        }
+
+        protected override void OnOpen()
+        {
+            base.OnOpen();
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var wssv = new WebSocketServer(9001);
+            wssv.AddWebSocketService<SocketServer>("/");
+            wssv.Start();
+            Console.Read(); //Stops the debugger from exiting run mode
         }
     }
 }
