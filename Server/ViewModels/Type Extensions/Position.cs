d8c1c4b421a989426ac33500a840699a0ed629fe
Str1XHyper <tijnvanveghel@gmail.com>
2021-11-30 09:53:26 +0100

    Added Layer structure and some viewmodels

---
+++ Server/ViewModels/Type Extensions/Position.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ViewModels.Type_Extensions
+{
+    public class Position
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+    }
+}
---
+++ Server/ViewModels/Users/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ViewModels.Type_Extensions;
+
+namespace ViewModels.Users
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public Position Position { get; set; }
+        public int SceneID { get; set; }
+    }
+}
