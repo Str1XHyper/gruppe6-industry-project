9c7b71d1f4017dcdd48da0ccae33f937a1326ec3
RickvanHam <428294@student.fontys.nl>
2021-12-08 14:36:10 +0100

    Started On Avatar Customization

---
+++ Speeldorp/Assets/Scripts/AvatarCustomizationManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AvatarCustomizationManager : MonoBehaviour
+{
+    [Header("Background Colors")]
+    [SerializeField]
+    private List<Color32> backgroundColors;
+    public GameObject colorpreview;
+    public GameObject iconBackground;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
