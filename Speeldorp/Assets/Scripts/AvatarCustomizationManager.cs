9280fa046f33eb2e992239be7ffe9ea3d646cce6
RickvanHam <428294@student.fontys.nl>
2021-12-10 09:45:40 +0100

    Fixed Unityengine.UI bug
    As well as removing visual studio code stuff

--- Speeldorp/Assets/Scripts/AvatarCustomizationManager.cs
+++ Speeldorp/Assets/Scripts/AvatarCustomizationManager.cs
@@ -8,17 +8,21 @@ public class AvatarCustomizationManager : MonoBehaviour
     [Header("Background Colors")]
     [SerializeField]
     private List<Color32> backgroundColors;
-    public GameObject colorpreview;
-    public GameObject iconBackground;
+    public RawImage colorpreview;
+    public RawImage backgroundImage;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        SetColorToPreview();
+    }
+
+    void SetColorToPreview()
+    {
+        backgroundImage.color = colorpreview.color;
     }
 }
